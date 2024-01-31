424a419306066bb6824e2b958956bb9825d6897c
hassan2314 <hassan87691@gmail.com>
2024-01-31 13:45:02 +0500

    Logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,2 @@
-<img src="{{ asset('images/logo.png') }}" alt="Customer Logo" style="background-color: yellow; height: 200px;margin-top: -30px;; border-radius:60%;">
+<img src="{{ asset('images/logo.png') }}" alt="Customer Logo"
+        style="background-color: white; height: 100px ;padding : 0px;margin-top: 10px;; border-radius:60%;">
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,6 @@
 <a href="/">
-   
-<img src="{{ asset('images/logo.png') }}" alt="Customer Logo" style="background-color: yellow; height: 140px ;padding : 0px;margin-top: -10px;; border-radius:60%;">
-</a>
+
+    <img src="{{ asset('images/logo.png') }}" alt="Customer Logo"
+        style="background-color: white; height: 100px ;padding : 0px;margin-top: 10px;; border-radius:60%;">
+
+    </a>
